6969dc6cf71c929cb7bc9ebca4be45c8e20b3428
kdchurch1994 <kdchurch@bsu.edu>
2021-08-25 00:37:41 -0400

    created a failing test to test the getName() function

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,5 @@
 //allows us to refer to Employee.js to test if the Employee constructor is working properly
+const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 //The first test will determine if a new employee object is working by referring to the employee class constructor
@@ -9,4 +10,13 @@ test('Creates an employee object',() => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+//Testing the getName function that should return the name that is entered.
+test('Gets the Employee name', () => {
+    const employee = new Employee('Kyle', 1, 'kdchurch@bsu.edu');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+
 
