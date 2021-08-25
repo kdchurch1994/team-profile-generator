dcbde4d935c5dc73c8d5ed0d0e5ddaeb1e80d639
kdchurch1994 <kdchurch@bsu.edu>
2021-08-25 04:15:05 -0400

    completed getSchool() and getRole() functions for Intern.js and created passing tests for each

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -8,3 +8,17 @@ test('Creates Intern object', () => {
     expect(intern.school).toEqual(expect.any(String));
 });
 
+//Test checks if the getSchool() function is getting school that is being attended by the Intern
+test('Gets the school name of the Intern', () => {
+    const intern = new Intern('Kyle', 1, 'kdchurch@bsu', 'Ball State University');
+
+    expect(intern.getSchool()).toEqual(expect.any(String));
+});
+
+//Test checks if the gitRole() function is getting the role of the Engineer and returning "Engineer" as the role
+test('Gets the Role of the Engineer', () => {
+    const intern = new Intern('Kyle', 1, 'kdchurch@bsu.edu', 'Ball State University');
+
+    expect(intern.getRole()).toEqual("Intern");
+});
+
