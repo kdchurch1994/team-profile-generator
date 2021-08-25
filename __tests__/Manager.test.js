46ac53c207962d32efd26ea99a918f33adaab1d2
kdchurch1994 <kdchurch@bsu.edu>
2021-08-25 01:43:29 -0400

    passing test for getRole() function for the Manager constructor that returns Manager as the role

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,5 +1,5 @@
 //allows us to refer to Manager.js to test if the Manager constructor is working properly
-const { test } = require('@jest/globals');
+const { test, expect } = require('@jest/globals');
 const Manager = require('../lib/Manager.js');
 
 //Test that checks if the new manager object is created successfully based on the Employee and Manager constructors
@@ -7,4 +7,11 @@ test('Creates a Manager object', () => {
     const manager = new Manager('Kyle', 1, 'kdchurch@bsu.edu', 98);
 
     expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+//Test checks if the getRole() function is getting the role of the manager and returning "Manager" as the role
+test('Gets the Role of the Manager',() => {
+    const manager = new Manager('Kyle', 1, 'kdchurch@bsu.edu', 98);
+
+    expect(manager.getRole()).toEqual("Manager");
 });
