8b2bd6cea7a08e1e68d71e13f355cd8700470d2f
kdchurch1994 <kdchurch@bsu.edu>
2021-08-25 02:41:47 -0400

    Engineer functions created and tests passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,4 @@
-const { test } = require('@jest/globals');
+const { test, expect } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
 
 //Test that checks if the new engineer object is created successfully based on the Employee and Engineer constructors
@@ -6,4 +6,20 @@ test('Creates Engineer object', () => {
     const engineer = new Engineer('Kyle', 1, 'kdchurch@bsu.edu', 'kdchurch1994');
 
     expect(engineer.github).toEqual(expect.any(String));
+});
+
+//
+
+//Test checks if the getGithub() function is getting the github username of the Engineer 
+test('Gets the github username of the Engineer', () => {
+    const engineer = new Engineer('Kyle', 1, 'kdchurch@bsu.edu', 'kdchurch1994');
+
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+});
+
+//Test checks if the gitRole() function is getting the role of the Engineer and returning "Engineer" as the role
+test('Gets the Role of the Engineer', () => {
+    const engineer = new Engineer('Kyle', 1, 'kdchurch@bsu.edu', 'kdchurch1994');
+
+    expect(engineer.getRole()).toEqual("Engineer");
 });
