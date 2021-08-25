5143d182c16446b9c02847cb99c909bbb0e1b2f2
kdchurch1994 <kdchurch@bsu.edu>
2021-08-25 02:06:19 -0400

    Wrote Failing test for Engineer model creation'

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,9 @@
+const { test } = require('@jest/globals');
+const Engineer = require('./Engineer');
+
+//Test that checks if the new engineer object is created successfully based on the Employee and Engineer constructors
+test('Creates Engineer object', () => {
+    const engineer = new Engineer('Kyle', 1, 'kdchurch@bsu.edu', 'kdchurch1994');
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,5 +1,4 @@
 //allows us to refer to Manager.js to test if the Manager constructor is working properly
-const { test, expect } = require('@jest/globals');
 const Manager = require('../lib/Manager.js');
 
 //Test that checks if the new manager object is created successfully based on the Employee and Manager constructors
