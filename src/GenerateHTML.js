f47e083ad0f95352130a98903299a22e38f2d2d5
kdchurch1994 <kdchurch@bsu.edu>
2021-08-27 01:26:06 -0400

    created two functions; one will generate the html for an intern and the other will generate the html for the engineer

--- src/GenerateHTML.js
+++ src/GenerateHTML.js
@@ -1,10 +1,11 @@
-const manageGenerate = function (manager) {
+//Function that is used to generate the HTML that will use the information entered into the prompt by the user when asked to add a manager
+const manageGenerate = function (manager) {//Materialize is being used to create an icon that will be displayed after the name of the manager is displayed
     return `
     <div class ="col-4">
         <div class="card">
             <div class="header-card">
-                <h3>${manager.managerName}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+                <h2>${manager.managerName}</h2>
+                <h3>Manager</h3><i class="material-icons">content_paste</i>
             </div>
 
             <div class="body-card">
@@ -14,5 +15,45 @@ const manageGenerate = function (manager) {
             </div>
         </div>
     </div>
+    `;
+}
+
+//Function that is used to generate the HTML that will use the information entered into the prompt by the user when asked to add a new employee and an intern is selected
+const Interngenerate = function (intern) {//Materialize is being used to create an icon that will be displayed after the name of the intern is displayed
+    return `
+    div class="col-4"
+        <div class="card">
+            <div class="header-card">
+                <h2>${intern.employeeName}</h2>
+                <h3>Intern</h3><i class="material-icons">assignment_ind</i>
+            </div>
+
+            <div class="body-card">
+                <p class="employee-ID">ID: ${intern.employeeID}</p>
+                <p class="employee-email">Email: <a href="mailto:${intern.employeeEmail}">${intern.employeeEmail}</a></p>
+                <p class="intern-school">School: ${intern.internSchool}</p>
+            </div>
+        </div>
+    </div>    
+    `;
+}
+
+//Function that is used to generate the HTML that will use the information entered into the prompt by the user when asked to add a new employee and an engineer is selcted
+const Engineergenerate = function (engineer) {//Materialize is being used to create an icon that will be displayed after the name of the engineer is displayed
+    return `
+    <div class="col-4">
+        div class="card">
+            <div class="header-card">
+                </h2>${engineer.employeeName}</h2>
+                <h3>Engineer</h3><i class="material-icons">laptop_mac</i>
+            </div>
+
+            <div class="body-card">
+                <p class="employee-ID">ID: ${engineer.employeeID}</p>
+                <p class="employee-email">Email: <a href="mailto:${engineer.employeeEmail}">${engineer.employeeEmail}</a></p>
+                <p class="engineer-github">GitHub Username: <a href="https://github.com/${engineer.engineerGithub}</a></p>
+            </div>
+        </div>
+    <div>    
     `;
 }
